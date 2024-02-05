274758454fa3fad0104a734a77c72802c24d61c0
rsanders <sandersrylan2@gmail.com>
2024-02-04 20:07:16 -0600

    Make the Calendar look a little nicer

---
+++ ProjectManager/Converters/CalendarDayColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace ProjectManager.Converters
+{
+    class TodayColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+            if (date.Day == DateTime.Today.Day)
+                return Brushes.Gray;
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}

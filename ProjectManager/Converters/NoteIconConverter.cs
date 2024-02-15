09a39bd5ba18ccfce4d21d608a5daf3b48677928
rsanders <sandersrylan2@gmail.com>
2024-02-14 19:54:03 -0600

    Add Icons to the note panel

---
+++ ProjectManager/Converters/NoteIconConverter.cs
@@ -0,0 +1,34 @@
+﻿using MaterialDesignThemes.Wpf;
+using ProjectManager.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ProjectManager.Converters
+{
+    public class NoteIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var noteType = (DataObjects.NoteDO.NoteType)value;
+            if (noteType == DataObjects.NoteDO.NoteType.Note)
+            {
+                return PackIconKind.Note;
+            }
+            else if (noteType == DataObjects.NoteDO.NoteType.Folder)
+            {
+                return PackIconKind.Folder;
+            }
+            return PackIconKind.Abacus;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

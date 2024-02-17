d649574c662530481b5963dcf744729ff74dc1ed
rsanders <sandersrylan2@gmail.com>
2024-02-17 14:39:10 -0600

    Prevent binding failures when loading the Notes Page

--- ProjectManager/Converters/TreeViewItemWidthConverter.cs
+++ ProjectManager/Converters/TreeViewItemWidthConverter.cs
@@ -12,7 +12,8 @@ namespace ProjectManager.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((double)value) / 2;
+            if((double)value==0)return 0;
+            return ((double)value) / 2 - 30;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
